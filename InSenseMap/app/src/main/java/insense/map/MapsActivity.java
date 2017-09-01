f0731e3a24e94dfd14b36dee0e50fc92b6e1b9c1
Matheus Costa Vieira <matheus.costa.vieira@gmail.com>
2017-09-01 14:54:17 -0300

    Add some maps interations

--- InSenseMap/app/src/main/java/insense/map/MapsActivity.java
+++ InSenseMap/app/src/main/java/insense/map/MapsActivity.java
@@ -1,18 +1,107 @@
 package insense.map;
 
-import android.support.v4.app.FragmentActivity;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.provider.SyncStateContract;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.io.IOException;
+import java.util.List;
+
+public class MapsActivity extends AppCompatActivity implements
+        OnMapReadyCallback,
+        ActivityCompat.OnRequestPermissionsResultCallback {
+
+    /**
+     * A {@link LocationSource} which reports a new location whenever a user long presses the map
+     * at
+     * the point at which a user long pressed the map.
+     */
+    private static class LongPressLocationSource implements LocationSource, GoogleMap.OnMapLongClickListener {
+
+        private OnLocationChangedListener mListener;
+
+        /**
+         * Flag to keep track of the activity's lifecycle. This is not strictly necessary in this
+         * case because onMapLongPress events don't occur while the activity containing the map is
+         * paused but is included to demonstrate best practices (e.g., if a background service were
+         * to be used).
+         */
+        private boolean mPaused;
+
+        @Override
+        public void activate(OnLocationChangedListener listener) {
+            mListener = listener;
+        }
+
+        @Override
+        public void deactivate() {
+            mListener = null;
+        }
+
+        @Override
+        public void onMapLongClick(LatLng point) {
+            if (mListener != null && !mPaused) {
+                Location location = new Location("LongPressLocationProvider");
+                location.setLatitude(point.latitude);
+                location.setLongitude(point.longitude);
+                location.setAccuracy(100);
+                mListener.onLocationChanged(location);
+            }
+        }
+
+        public void onPause() {
+            mPaused = true;
+        }
+
+        public void onResume() {
+            mPaused = false;
+        }
+    }
 
     private GoogleMap mMap;
+    private EditText locationSearch;
+
+    private Geocoder geocoder;
+
+    private LocationManager locationManager;
+
+    private LongPressLocationSource mLocationSouce;
+
+    /**
+     * Request code for location permission request.
+     *
+     * @see #onRequestPermissionsResult(int, String[], int[])
+     */
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    /**
+     * Flag indicating whether a requested permission has been denied after returning in
+     * {@link #onRequestPermissionsResult(int, String[], int[])}.
+     */
+    private boolean mPermissionDenied = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +111,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
 
+
+        locationSearch = (EditText) findViewById(R.id.editText);
+
+        geocoder = new Geocoder(this);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+    }
 
     /**
      * Manipulates the map once available.
@@ -38,9 +133,95 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        enableMyLocation();
+
+        mMap.setLocationSource(mLocationSouce);
     }
+
+    /**
+     * Enables the My Location layer if the fine location permission has been granted.
+     */
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+
+            Criteria criteria = new Criteria();
+            String provider = locationManager.getBestProvider(criteria, true);
+            Location location = locationManager.getLastKnownLocation(provider);
+
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+
+            mMap.animateCamera(CameraUpdateFactory
+                    .newLatLngZoom(new LatLng(latitude, longitude), 21));
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation();
+        } else {
+            // Display the missing permission error dialog when the fragments resume.
+            mPermissionDenied = true;
+        }
+    }
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (mPermissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError();
+            mPermissionDenied = false;
+        }
+    }
+
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private void showMissingPermissionError() {
+        PermissionUtils
+                .PermissionDeniedDialog
+                .newInstance(true)
+                .show(getSupportFragmentManager(), "dialog");
+    }
+
+    public void onMapSearch(View view) {
+        String location = locationSearch.getText().toString();
+        List<Address> addressList = null;
+
+        if (location.isEmpty())
+            return;
+        try {
+            addressList = geocoder.getFromLocationName(location, 1);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (addressList == null || addressList.isEmpty())
+            return;
+
+        Address address = addressList.get(0);
+        LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+    }
+
 }
